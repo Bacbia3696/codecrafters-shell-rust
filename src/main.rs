37754a7d739d566cca460e6ccd85628b836078d2
dat-thanh-nguyen-gfg <dat.nguyenthanh@global-fashion-group.com>
2026-02-28 13:39:58 +0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,12 @@
 use std::io::{self, Write};
 
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    let mut command = String::new();
+    io::stdin()
+        .read_line(&mut command)
+        .expect("Failed to read line");
+    println!("{}: command not found", command.trim());
 }
